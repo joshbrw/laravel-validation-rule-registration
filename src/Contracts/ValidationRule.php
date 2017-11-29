f955468d4c68dae42e5689ead52f2e7908cd5575
Josh Brown <josh@kalexiko.com>
2017-11-29 15:51:42 +0000

    Attributes can be either a string or an array.

--- src/Contracts/ValidationRule.php
+++ src/Contracts/ValidationRule.php
@@ -9,12 +9,12 @@ interface ValidationRule
     /**
      * Validate an attribute
      * @param string $attribute The attribute's name
-     * @param string $value The attribute's value
+     * @param string|array $value The attribute's value
      * @param array $parameters Any parameters passed to the validation rule
      * @param Validator $validator The validator instance
      * @return bool
      */
-    public function validate(string $attribute, string $value, array $parameters, Validator $validator): bool;
+    public function validate(string $attribute, $value, array $parameters, Validator $validator): bool;
 
     /**
      * Run any required error message string replacements for this validation rule.
